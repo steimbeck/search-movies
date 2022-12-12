4e6d17e4334f0a0b7a0968318b90acf88f26104d
steimbeck <steinbeck.ehue@gmail.com>
2022-12-12 23:17:14 +0100

    fetchData

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'b7be2e8b',
+            s: searchTerm
+
+        }
+
+    });
+
+    console.log(response.data)
+
+};
+
+
+const input = document.querySelector('input');
+const debounce = (func) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, 1000);
+    };
+
+};
+const onInput = event => {
+    fetchData(event.target.value);
+};
+input.addEventListener('input', debounce(onInput));
