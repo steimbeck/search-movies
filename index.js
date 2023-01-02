d84fc4e5cddfecd453bd13b07118ade02f7df48a
steimbeck <steinbeck.ehue@gmail.com>
2023-01-02 21:53:11 +0100

    stylescss

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ const fetchData = async (searchTerm) => {
         }
 
     });
+    if (response.data, Error) {
 
-    console.log(response.data)
+        return [];
+    }
 
+    return response.data.Search;
 };
 
 
 const input = document.querySelector('input');
-const debounce = (func, delay = 1000) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            func.apply(null, args);
-        }, delay);
-    };
 
-};
-const onInput = event => {
-    fetchData(event.target.value);
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    for (let movie of movies) {
+
+        const div = document.createElement('div');
+        div.innerHTML = ` <img src ="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+ `;
+        document.querySelector('#target').appendChild(div);
+    }
 };
 input.addEventListener('input', debounce(onInput, 500));
