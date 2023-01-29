00a2ed981e80aace310d36992baa36ae865382a5
steimbeck <steinbeck.ehue@gmail.com>
2023-01-29 19:07:36 +0100

    end1

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ const onInput = async event => {
         <img src ="${imgSrc}" />
         ${movie.Title}
  `;
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+            onMovieSelect(movie);
+
+        });
+
         resultsWrapper.appendChild(option);
     }
 };
@@ -61,4 +68,65 @@ document.addEventListener('click', event => {
     }
 
 
-})
+});
+
+const onMovieSelect = async movie => {
+    const response = await axios.get('http://www.omdbapi.com', {
+        params: {
+            apikey: 'b7be2e8b',
+            i: movie.imdbID
+        }
+
+    });
+
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+    return `
+<article class="media">
+<figure class="media-left">
+<p class= "image">
+<img src = "${movieDetail.Poster}">
+</p>
+</figure>
+<div class="media-content">
+<div class="content">
+<h1>${movieDetail.Title}</h1>
+<h4>${movieDetail.Genre}</h4>
+<p>${movieDetail.Plot}</p>
+</div>
+</div>
+    </article>
+<article class= "notification is - primary">
+<p class="title">${movieDetail.Awards}</p>
+<p class="subtitle">Awards<p>
+</article>
+
+</article>
+<article class= "notification is - primary">
+<p class="title">${movieDetail.BoxOffice}</p>
+<p class="subtitle">BoxOffice<p>
+</article>
+
+</article>
+<article class= "notification is - primary">
+<p class="title">${movieDetail.Metascore}</p>
+<p class="subtitle">Metascore<p>
+</article>
+</article>
+
+<article class= "notification is - primary">
+<p class="title">${movieDetail.imdRating}</p>
+<p class="subtitle">imdRating<p>
+</article>
+
+</article>
+<article class= "notification is - primary">
+<p class="title">${movieDetail.imdbVotes}</p>
+<p class="subtitle">imdbVotes<p>
+</article>
+
+`;
+};
+
